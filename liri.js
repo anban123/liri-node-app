2c5b48d816d714a2e66a02cf12964489f2dc65c7
Anna Grant <anban123@msn.com>
2019-06-26 18:42:02 -0600

    updating everything

---
+++ liri.js
@@ -0,0 +1,99 @@
+require("dotenv").config();
+
+var axios = require("axios");
+var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+
+var action = process.argv[2];
+
+//need moment.js link
+
+//Liri Commands:
+// concert-this          node liri.js concert-this <artist/band name here>
+// spotify-this-song     node liri.js spotify-this-song '<song name here>'
+// movie-this            node liri.js movie-this '<movie name here>'
+// do-what-it-says       node liri.js do-what-it-says
+
+
+//function for bandsintown
+var input = process.argv.slice(3).join("+");
+
+function bit(bitInput) {
+    axios.get("https://rest.bandsintown.com/artists/" + bitInput + "/events?app_id=codingbootcamp")
+        .then (function(response) {
+            //console.log("BIT Data: ", response.data[0]);   //ojects or arrays
+    
+            console.log(`Name of Venue: ${response.data[0].venue.name}`);
+            console.log(`Venue Location: ${response.data[0].venue.country}`);
+            console.log(`Event Date: ${response.data[0].datetime}`)
+        
+});
+};
+
+//function for spotify
+function spot() {
+    var spotSong = process.argv[3];
+    
+    spotify.search({ type: 'track', query: spotSong }, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+        console.log(`Name of Artist or Band: ` + data.tracks.items[0].artists[0].name);
+        console.log(`Name of the Song: ` + data.tracks.items[0].name);
+        console.log(`Song Preview: ` + data.tracks.items[0].external_urls.spotify);
+        console.log(`Album: ` + data.tracks.items[0].album.name);
+       
+       //console.log(data.tracks.items[0].external_urls.spotify); 
+      });       
+};
+
+//function for OMDB
+function o() {
+    var oTitle = process.argv.slice(3).join("+");
+
+    if (oTitle === undefined) {
+        oTitle = "Mr. Nobody"
+    }
+
+    axios.get("http://www.omdbapi.com/?t=" + oTitle + "&y=&plot=short&apikey=trilogy")
+        .then (function(response) {
+            //console.log(response.data);
+  
+            
+            console.log(`Movie Title: ${response.data.Title}`);
+            console.log(`IMDB Rating: ${response.data.Ratings[0].Value}`);
+            console.log(`Rotten Tomatoes Rating: ${response.data.Ratings[1].Value}`);
+            console.log(`Country of Production: ${response.data.Country}`);
+            console.log(`Language: ${response.data.Language}`);
+            console.log(`Plot: ${response.data.Plot}`);
+            console.log(`Actors: ${response.data.Actors}`);
+        
+        });
+     }
+// //Function for Do What it Says 
+// function dwis() {
+    //fs.readFile
+
+//     var split (",");
+//     var song = 
+//     var command = 
+// }
+
+
+function Switch(act, inp) {
+switch(act) {
+    case "concert-this":
+        return bit(inp);
+    case "spotify-this-song":
+        return spot(inp);
+    case "movie-this":
+        return o(inp);
+    case "do-what-it-says":
+        return dwis();
+    default:
+        return console.log("You're doing it wrong!!!")
+}
+}
+
+Switch(action, input)
